6fc1ca0badb2ab1a1c2544d2f38ce383c720e5c5
XiaoWind <zxiaowind@qq.com>
2019-08-18 17:48:44 +0800

    ep to url

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 ``0 3 * * * cd /home/dir/getop && python getop.py >/dev/null 2>&1``
 
 update:
+<<<<<<< HEAD
+=======
+20190818:将记录集数修改成记录链接地址。
+
+>>>>>>> ep to url
 20190108:枫雪动漫改用WordPress，开启了防爬虫，增加headers模拟。
 
 20180703:增加对V2种子的判断，只要标题带V2就下载种子。
--- getop.py
+++ getop.py
@@ -14,26 +14,43 @@ def main():
     if not os.path.exists('torrent'):
         os.makedirs('torrent')
 
-    #读取已下载过种子的集数保存到ep
+    #读取已下载过种子的链接，保存到ep
     with open('ep.txt','r') as f:
-        ep = int(f.read())
+        ep = f.read()
 
+<<<<<<< HEAD
     url = "http://www.opfans.org/" #?cat=1,FINAL分类
+=======
+    url = "http://www.opfans.org/?cat=1" #?cat=1,FINAL分类
+>>>>>>> ep to url
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
     html = response.read()
     #print html
 
+<<<<<<< HEAD
     links = re.finditer(r'<a href="([^>]*?)"[^>]*第(.*?)话.*?FINAL(.*?)>',html)
     for i in links:
         if "V2" in i.group(3) or int(i.group(2)) > ep:
             url = i.group(1)
             print url
+=======
+    links = re.finditer(r'<a href="([^>]*?)"[^>]*第(.*?话).*?FINAL(.*?)>',html)
+
+    #将迭代器转成list
+    links = list(links)
+
+    for i in links:
+        if i.group(1) not in ep:
+            url = i.group(1)
+            #print url
+>>>>>>> ep to url
 
             request = urllib2.Request(url)
             response = urllib2.urlopen(request)
             html = response.read()
 
+<<<<<<< HEAD
             link = re.search(r'<a href="(.*?)".*?下载地址',html)
             link = link.group(1)
             print link
@@ -48,19 +65,22 @@ def main():
             link = re.search(r'href="(.*?torrent)"',html)
             link = link.group(1)
             print link
+=======
+            link = re.search(r'href="(.*?torrent)"',html)
+            link = link.group(1)
+            #print link
+>>>>>>> ep to url
 
             dd = urllib2.urlopen(link)
-            isv2 = ""
-            if "V2" in i.group(3):
-                isv2 = "v2"
-            with open("torrent/"+i.group(2)+isv2+".torrent","wb") as f:
+            with open("torrent/"+i.group(2)+url.partition("=")[2]+".torrent","wb") as f:
                 f.write(dd.read())
-            #种子下载完成，只有当前集数较新时才写入ep.txt
-            with open('ep.txt','r') as f:
-                tempep = int(f.read())
-            if int(i.group(2)) > tempep:
-                with open('ep.txt','w') as f:
-                    f.write(i.group(2))
+
+    #种子下载完成，将所有已下种子的链接写入ep.txt
+    tempep = ""
+    for i in links:
+        tempep = tempep + i.group(1) + "\n"
+    with open('ep.txt','w') as f:
+        f.write(tempep)
 
 if __name__ == '__main__':
     main()
